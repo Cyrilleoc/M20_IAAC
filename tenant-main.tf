3f7c34419dd5eaf7e58890c89f69dd483300fed9
Cyrille85 <nonvignoncyrille3@gmail.com>
2021-07-15 14:52:59 -0400

    newCidr

--- tenant-main.tf
+++ tenant-main.tf
@@ -5,8 +5,6 @@ terraform {
       version = "~> 3.27"
     }
   }
-
-  required_version = ">= 0.14.9"
 }
 
 provider "aws" {
@@ -35,7 +33,7 @@ resource "aws_vpc" "tenant_vpc" {
 # this is not part of cloudformation template, for demo purpose only
 resource "aws_subnet" "tenant_public_subnet" {
   vpc_id     = aws_vpc.tenant_vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.1.0/16"
 
   map_public_ip_on_launch = true // it makes this a public subnet
   availability_zone = "us-east-1a"
