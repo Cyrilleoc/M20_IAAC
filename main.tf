a1c6e761e617d825f5f9e6df76e433e31c12f491
Nonvignon <Nonvignon.C.Oussou@leidos.com>
2021-10-19 14:19:53 -0400

    comments added

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+
+# Call Tenant module
 module "tenant" {
   source = "./modules/tenant"
   tenant_vpc_name = var.tenant_vpc_name
@@ -6,15 +8,14 @@ module "tenant" {
   peer_vpc_id = var.peer_vpc_id
   peer_role_arn = var.peer_role_arn
   peer_vpc_account_id = var.peer_vpc_account_id
-
-
-
 }
 
+# Call IAM module
 # module "iam" {
 #   source = "./modules/iam"
 # }
 
+# Call Payer module
 # module "payer" {
 #   source = "./modules/payer"
 #   billing_bucket = "payeraccount1"
